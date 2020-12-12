8f08b5fe5a4433db342591ba9bb4a995a1d6ba03
samHince <sam.hince7@gmail.com>
2020-12-11 18:33:17 -0600

    Completed homework 7.

--- MAE170HW7.m
+++ MAE170HW7.m
@@ -10,8 +10,42 @@
 % discussions and i dont even know what lectures are at this point. Its
 % like watching a fish bowl. 
 
+%% problem 8.3
+
+clc;
+clear;
+
+sys = tf([4,11.18],[1,4,11.18])
+bode(sys)
+
+%% problem 3a
+
 clc;
 clear;
 
 sys = tf([1],[0.75,3.25,1])
+bode(sys)
+
+%% problem 3b
+
+clc;
+clear;
+
+sys = tf([0.1,1],[1,0,0])
+bode(sys)
+
+%% problem 3c
+
+clc;
+clear;
+
+sys = tf([1,-5],[1,3,5])
+bode(sys)
+
+%% problem 3d
+
+clc;
+clear;
+
+sys = tf([50,300],[1,11,24,0])
 bode(sys)
---
+++ MAE170HW8.m
@@ -0,0 +1,12 @@
+% MAE 170 HW 8
+
+% Sam Hince 
+% 19295309
+
+%% problem 8.3
+
+clc;
+clear;
+
+sys = tf([4,11.18],[1,4,11.18])
+bode(sys)
